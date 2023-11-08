7b487e8e10d79a7c88c44843c8473fb89895a416
sjpau <theonlymoby@protonmail.com>
2023-11-08 17:14:55 +0200

    working downloader functionality

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -14,7 +18,7 @@ import (
 
 var (
 	TOR_PROXY     string = "socks5://127.0.0.1:9050"
-	URL           string = "http://libraryfyuybp7oyidyya3ah5xvwgyx6weauoini7zyz555litmmumad.onion/"
+	URL           string = "http://libraryfyuybp7oyidyya3ah5xvwgyx6weauoini7zyz555litmmumad.onion"
 	STRING_SEARCH string = "s?query="
 	STRING_TYPE   string = "src="
 )
@@ -31,50 +35,121 @@ func main() {
 
 	var client *http.Client
 	var resp *http.Response
-	var req *http.Request
 	var err error
 
 	log.Println(client, resp)
 
 	InitCmdOptions()
-	if FlagProxy == "" {
-		client = NewTorProxyClient(TOR_PROXY)
-	} else {
-		client = NewTorProxyClient(FlagProxy)
+	if FlagURL == "" {
+		FlagURL = URL
 	}
-	log.Printf("Client is set.\n")
+	if FlagProxy == "" {
+		FlagProxy = TOR_PROXY
+	}
 	if FlagSrc == "" {
 		FlagSrc = "bk"
 	}
+	client = NewTorProxyClient(FlagProxy)
+	log.Printf("Client is set.\n")
 	log.Printf("Setting request parameters...\n")
 	searchUrl := JoinSearchUrl()
-	req, err = http.NewRequest("GET", searchUrl, nil)
+	req, err := CreateRequest(searchUrl, "GET")
 	Check(err)
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("Sec-Fetch-Dest", "document")
-	req.Header.Set("Sec-Fetch-Mode", "navigate")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-User", "?1")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/115.0")
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
-
 	log.Printf("Sending request...\n")
-	//resp, err = client.Do(req)
-	//Check(err)
-	//defer resp.Body.Close()
+	resp, err = client.Do(req)
+	Check(err)
+	defer resp.Body.Close()
 	log.Printf("Body received.\n")
-	//body, err := io.ReadAll(resp.Body)
-	body, err := os.ReadFile("index.html")
+	body, err := io.ReadAll(resp.Body)
+	//body, err := os.ReadFile("index.html")
 	Check(err)
 	doc, err := html.Parse(strings.NewReader(string(body)))
 	Check(err)
-	log.Printf("Traversing HTML for class \"%s\"", SOURCES[FlagSrc])
-	elems := FindElementsByClass(doc, SOURCES[FlagSrc])
-	for i := 0; i < len(elems); i++ {
-		log.Println((FindElementFromAttr(elems[i].FirstChild.FirstChild, "img", "title")))
+	elems := MapElementsFormHTML(doc)
+	tits := PrintTitlesf(elems)
+	input := GetDownloadInput()
+	downloadIndicies := DownloadInputStringToIntSlice(input)
+	for i := 0; i < len(downloadIndicies); i++ {
+		log.Printf("Downloading %s\n", elems[tits[downloadIndicies[i]]])
+		DownloadWithClient(client, elems[tits[downloadIndicies[i]]], tits[downloadIndicies[i]])
+		log.Println("Finished.")
 	}
+}
+
+func DownloadInputStringToIntSlice(input string) []int {
+	parts := strings.Fields(input)
+	var numbers []int
+
+	for _, part := range parts {
+		number, err := strconv.Atoi(part)
+		if err != nil {
+			fmt.Printf("Error converting %s to an integer: %v\n", part, err)
+			continue
+		}
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+func GetDownloadInput() string {
+	fmt.Print("Indices to download (ex.: 1 4 12 2): ")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input.", err)
+		return ""
+	}
+
+	input = strings.TrimSuffix(input, "\n")
+	return input
+}
+
+func PrintTitlesf(elems map[string]string) []string {
+	var titles []string
+	for k := range elems {
+		titles = append(titles, k)
+	}
+	for i := 0; i < len(titles); i++ {
+		fmt.Printf("[%d]: %s\n", i, titles[i])
+	}
+	return titles
+}
+
+func DownloadWithClient(client *http.Client, URL, fileName string) error {
+	req, err := CreateRequest(URL, "GET")
+	Check(err)
+	response, err := client.Do(req)
+	Check(err)
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		log.Printf("Received %d response code when downloading %s.", response.StatusCode, URL)
+		return errors.New("Download failed!")
+	}
+	file, err := os.Create(fileName)
+	Check(err)
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	Check(err)
+
+	return nil
+}
+
+func MapElementsFormHTML(doc *html.Node) map[string]string {
+	var titles []string
+
+	hrefs := FindElementsByClass(doc, SOURCES[FlagSrc])
+	l := len(hrefs)
+	for i := 0; i < l; i++ {
+		titles = append(titles, FindElementFromAttr(hrefs[i].FirstChild.FirstChild, "img", "title"))
+	}
+
+	elems := make(map[string]string, l)
+	for i := 0; i < l; i++ {
+		elems[titles[i]] = fmt.Sprintf("%s%s", FlagURL, FindElementFromAttr(hrefs[i], "a", "href"))
+	}
+	return elems
 }
 
 func FindElementsByClass(n *html.Node, targetClass string) []*html.Node {
@@ -126,6 +201,24 @@ func FindElementFromAttr(node *html.Node, data string, key string) string {
 	return value
 }
 
+func CreateRequest(URL, r string) (*http.Request, error) {
+	req, err := http.NewRequest(r, URL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/115.0")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.5")
+
+	return req, nil
+}
+
 func NewTorProxyClient(proxyPath string) *http.Client {
 	log.Printf("Proxy path: %s\n", proxyPath)
 	torProxyUrl, err := url.Parse(proxyPath)
@@ -145,12 +238,7 @@ func JoinSearchUrl() string {
 	if FlagQuery == "" {
 		log.Fatal("Query is empty!\n")
 	}
-	if FlagURL == "" {
-		searchUrl = URL
-	} else {
-		searchUrl = FlagURL
-	}
-	searchUrl = fmt.Sprintf("%s%s%s%s%s%s", searchUrl, STRING_SEARCH, FlagQuery, "&", STRING_TYPE, FlagSrc)
+	searchUrl = fmt.Sprintf("%s%s%s%s%s%s%s", FlagURL, "/", STRING_SEARCH, FlagQuery, "&", STRING_TYPE, FlagSrc)
 	log.Printf("Joined URL: %s\n", searchUrl)
 	return searchUrl
 }
